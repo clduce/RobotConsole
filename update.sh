bf0a94c76b891a9c9274a00155310619fec27b04
markskinner92 <markhskinner@gmail.com>
2021-04-15 13:10:47 -0600

    updated update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
-cp settings.json ~/settings.backup.json
+cp ~/catkin_ws/src/roboquest_ui/src/settings.json ~/settings.backup.json
+cd ~/catkin_ws/src/roboquest_ui/src
 git fetch
 git reset --hard HEAD
 git merge '@{u}'
 npm i
-cp ~/settings.backup.json settings.json
+cp ~/settings.backup.json ~/catkin_ws/src/roboquest_ui/src/settings.json
