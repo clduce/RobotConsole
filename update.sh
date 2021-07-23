3551cfc3410e068fc8934950f26f78ca1ffc6528
markskinner92 <markhskinner@gmail.com>
2021-07-23 12:59:57 -0600

    purged npm

--- update.sh
+++
@@ -1,7 +0,0 @@
-cp ~/catkin_ws/src/roboquest_ui/src/settings.json ~/settings.backup.json
-cd ~/catkin_ws/src/roboquest_ui/src
-git fetch
-git reset --hard HEAD
-git merge '@{u}'
-npm i
-cp ~/settings.backup.json ~/catkin_ws/src/roboquest_ui/src/settings.json
